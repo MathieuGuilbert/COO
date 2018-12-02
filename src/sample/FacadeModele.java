604fc3e40813248c8d725f497c218e1f91ca263f
Maëlys <44547926+MaelysG@users.noreply.github.com>
2018-12-02 17:45:36 +0100

    Add files via upload
    Butons directionnels fonctionnels

---
+++ src/sample/FacadeModele.java
@@ -0,0 +1,16 @@
+package sample;
+
+import javafx.util.Pair;
+
+public class FacadeModele {
+    ModeleConcret modele = new ModeleConcret();
+
+    public int[][] getEtat() { return modele.getEtat(); }
+
+    public void reset() { modele.reset(); }
+
+    public void right() { modele.right(); }
+    public void left() { modele.left(); }
+    public void up() { modele.up(); }
+    public void down() { modele.down(); }
+}
